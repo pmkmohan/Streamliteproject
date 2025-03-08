5f7ab4163987ec414a192b063e6890f84bb2ce99
Mohan P <pmk.mohan@gmail.com>
2025-03-08 17:00:41 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 st.write(" My first app Hello Mohan")
 
-df = pd,read_csv("E:\NareshIT\Projects\KaggleProjects\movelens-20m-datase\genome_scores.csv")
+df = pd,read_csv(r"E:\NareshIT\Projects\KaggleProjects\movelens-20m-datase\genome_scores.csv")
 st.line_chart(df)
